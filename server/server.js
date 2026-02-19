2e6a7894aac5460f912a7b19ca551581e78ed65f
mwaqas122 <muhammad.waqas@admaxim.com>
2026-02-19 10:55:28 +0500

    Use environment port for Render

--- server/server.js
+++ server/server.js
@@ -43,8 +43,13 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("🚀 Server running at http://localhost:3000");
+// server.listen(3000, () => {
+//   console.log("🚀 Server running at http://localhost:3000");
+// });
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`🚀 Server running at port ${PORT}`);
 });
 
 
